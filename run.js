419994b11d67eadd1542ac6a7c282a745166a060
liukan <city.of.beijing@gmail.com>
2016-04-02 10:43:52 +0800

    update the image handler

--- run.js
+++ run.js
@@ -1,56 +1,91 @@
 /**
  * Created by jerry on 4/1/16.
  */
-
-var ImageProcessor = require("./processImage");
-var args = process.argv;
-
-function usage(){
-    console.log("==========usage=============");
-    console.log("node run.js <folder>");
-};
-
-if(args.length != 3){
-    usage();
-    process.exit(-1);
-}
-
+var easyimg = require("easyimage");
 var path = require("path");
 var fs = require("fs");
+var config = require(path.join(process.env.HOME, ".aflodit.json"));
 
-function processFile(path){
-    //console.log("processing file " + path);
-    new ImageProcessor(path).process();
-    /*
-        .then(function(result){
-            console.log(result);
-        }, function(error){
-            console.log(error);
-        });
-        */
+var imageFolder = config.folder;
+var mongoUrl = config.mongo + "/" + config.database;
+//var mongoose = require("mongoose");
+//mongoose.connect(mongoUrl);
+
+/**
+ * process the list sequentially
+ * @param list
+ * @param iterator
+ */
+function sequentialList(list, iterator, callback){
+    _sequentialList(list, 0, iterator, callback);
 };
 
-function processDir(dirPath){
-    var files = fs.readdirSync(dirPath);
-    files.forEach(function(file){
-        var filePath = path.join(dirPath, file);
-        var stat = fs.lstatSync(filePath);
-        if(stat.isDirectory()){
-            processDir(filePath);
-        }else if(stat.isFile()){
-            processFile(filePath);
+function _sequentialList(list, i, iterator, callback){
+    if(i >= list.length){
+        return callback();
+    }
+
+    iterator(list[i], function(error){
+        if(error){
+            console.log("error: " + error);
+            return callback(error);
+        }else{
+            _sequentialList(list, i+1, iterator, callback);
         }
-    })
-}
+    });
+};
 
-var imageFolderPath = path.resolve(args[2]);
 
-//console.log("processing folder " + imageFolderPath);
+function _process(file, callback){
+    var stat = fs.lstatSync(file);
+    if(stat.isDirectory()){
+        processDir(file, callback);
+    }else if(stat.isFile()){
+        processFile(file, callback);
+    }
+};
 
-processDir(imageFolderPath);
+function processFile(path, callback){
+    console.log("process file " + path);
+    easyimg.info(path)
+        .then(
+            function(result){
+                var wDh = result.width / result.height;
+                if(wDh > 1){
+                    console.log("file " + path + " w/d is " + wDh);
+                }
+                callback();
+            },
+            function(error){
+                console.log("error in file " + path);
+                callback();
+            }
+        )
+};
+
+function processDir(dirPath, callback){
+    var files = fs.readdirSync(dirPath);
+
+    sequentialList(
+        files.map(function(file){
+            return path.join(dirPath, file);
+        })
+        , _process
+        , callback
+    );
+};
+
+var end = false;
+_process(imageFolder, function(error, result){
+    end = true;
+});
 
 function wait(){
-    setTimeout(wait, 10000);
+    if(end){
+        return ;
+    }else{
+        setTimeout(wait, 10000);
+    }
 }
 
-wait();
+//wait();
