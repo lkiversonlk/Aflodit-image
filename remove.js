b65d5a8ffa6112cfd210bf7f060808fef47a7354
liukan <city.of.beijing@gmail.com>
2016-04-02 12:31:19 +0800

    add remover

--- remove.js
+++ remove.js
@@ -3,8 +3,10 @@
  */
 var client = require("mongodb").MongoClient;
 var path = require("path");
-var config = require(path.join(process.env.HOME, ".aflodit.json"));
+var fs = require("fs");
 
+
+var config = require(path.join(process.env.HOME, ".aflodit.json"));
 var imageFolder = config.folder;
 var mongoUrl = "mongodb://" + config.mongo.mongourl + "/" + config.mongo.db;
 
@@ -29,6 +31,14 @@ client.connect(mongoUrl, function(err, db){
                 console.log("error: " + err);
             }else{
                 console.log(Date.now() + "delete " + result.result.n);
+                try{
+                    var filePath = path.join(imageFolder, fild_id.substr(0,2), fild_id);
+                    console.log("delete file " + filePath);
+                    fs.unlinkSync(filePath);
+                }catch (error){
+                    console.log("error: " + error);
+                }
+
             }
         });
     });
